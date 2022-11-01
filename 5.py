92f0b426d2f0b0bbebf852047a8ac31bfacbdca9
MateusViiana <101288993+MateusViiana@users.noreply.github.com>
2022-10-31 22:44:34 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,5 @@
+# -*-coding:latin1-*-
+frase = str(input('Digite uma frase: '))
+print(' Frase Digitada: {}'.format(frase))
+string = frase[::-1]
+print('o inverso da frase fica: {}'.format(string))
